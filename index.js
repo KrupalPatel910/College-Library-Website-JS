44b8f29f8e7f623f1853b0d07b4617c9ee8aa57c
Krupal Patel <krupal9102001@gmail.com>
2024-03-03 18:14:48 +0530

    Complete-Form-Section

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("This is index.js")
+
+
+// Constructor
+function Book(name, author, type){
+    this.name = name;
+    this.author = author;
+    this.type = type;
+
+}
+
+//Display Constructor
+
+
+
+//Add methods to display prototype
+
+
+
+// Add Submit event listener
